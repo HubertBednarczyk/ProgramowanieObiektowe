c5c1c02fa552aa780400480b026afd957a9096d6
Autentyk <162135@student.uwm.edu.pl>
2022-12-14 13:32:39 +0100

    update

--- Cwiczenia/9_cw/cw9/src/Zadanie2.java
+++ Cwiczenia/9_cw/cw9/src/Zadanie2.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Zadanie2 {
     public static void main(String[] args){
-
-        
+        Random r = new Random();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Podaj liczbe n: ");
+        int n;
+        n= s.nextInt();
+        int[] tab = new int[n];
+        for(int i =0;i<n;i++){
+            tab[i] = r.nextInt(1999)-999;
+        }
+        System.out.println(Arrays.toString(tab));
     }
 }
