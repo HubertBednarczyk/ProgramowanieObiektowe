2d3512f0712431374c5dca0dd45eed3f84775261
Autentyk <162135@student.uwm.edu.pl>
2023-01-04 18:23:45 +0100

    update(ucze_sie)

--- Cwiczenia/10_cw/Cw10/src/Main.java
+++ Cwiczenia/10_cw/Cw10/src/Main.java
@@ -1,12 +1,23 @@
 
 class Main {
-    public static int countChar(String str, char c){
+    public static int countChar(String str){
+        int ileC = 0;
+        for(int i =0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c=='c'){
+                ileC+=1;
+            }
+        }
+        return ileC;
+    }
 
-        return 0;
+    public static int countSubStr(String str, String subStr){
+
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String inputString = "Hello@%&Worldccc!#123";
+        System.out.println("Ilosc c w napisie: "+countChar(inputString));
     }
 }
