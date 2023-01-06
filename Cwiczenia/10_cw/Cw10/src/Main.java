6b82fe3b73510c16917d55544d203dee7e267d0e
Autentyk <162135@student.uwm.edu.pl>
2023-01-07 00:06:51 +0100

    update

--- Cwiczenia/10_cw/Cw10/src/Main.java
+++ Cwiczenia/10_cw/Cw10/src/Main.java
@@ -1,23 +1,41 @@
 
 class Main {
-    public static int countChar(String str){
+    public static int countChar(String str, char c){
         int ileC = 0;
         for(int i =0;i<str.length();i++){
-            char c = str.charAt(i);
-            if(c=='c'){
-                ileC+=1;
+            if(str.charAt(i) == c){
+                ileC += 1;
             }
         }
         return ileC;
     }
 
     public static int countSubStr(String str, String subStr){
+        int len = subStr.length();
+        int ilestr = 0;
+        for(int i = 0;i<str.length()-len+1;i++){
+
+        }
+        return ilestr;
+    }
+
+    public static String middle(String str){
+
+        return str.substring(2,3);
+    }
+
+    public static String repeat(String str, int n ){
 
     }
 
     public static void main(String[] args) {
 
-        String inputString = "Hello@%&Worldccc!#123";
+        String inputString = "Hello@%&Worldcccc!#123";
         System.out.println("Ilosc c w napisie: "+countChar(inputString));
+
+        String inputString2 = "HelloHelloHello2";
+        String inputString3 = "HelleHello2";
+        System.out.println("Ilosc napisu w napisie: "+countSubStr("HelloHelloHello2","Hello"));
+        System.out.println(middle("Middle"));
     }
 }
